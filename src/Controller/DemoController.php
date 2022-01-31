78e856ddde1f04c33d218ba75c64dc42de71c1ea
unknown <hanane.alaoui.foreach@gmail.com>
2022-01-31 13:53:03 +0100

    demo

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Demo;
+use App\Form\DemoType;
+use App\Entity\Message;
+use App\Form\FormulaireType;
 
 class DemoController extends AbstractController
 {
@@ -27,47 +31,58 @@ class DemoController extends AbstractController
 
     public function damier(): Response
     {
- 
-    $ligne = 8;
-    $colone = 8;
-    $html = '<table>';
-    for( $i=0 ; $i<$ligne ; $i++ )
-    {
-        // la ligne est-elle pair ?
-        if ( $i % 2 == 0 )
+        $ligne = 8;
+        $colone = 8;
+        $html = "<div>";
+        for( $i=0 ; $i<$ligne ; $i++ )
         {
-            $html .= "<tr>\n";
-            for( $j=0 ; $j < $colone ; $j++ )
+            // la ligne est-elle pair ?
+            if ( $i % 2 == 0 )
             {
-                // la colonne est-elle pair ?
-                if ( $j % 2 == 0 )
-                    $class="class='noir'";
-                else
-                    $class="class='blanc'";
-                    $html .= "<td $class>\n";
-                $html .= "</td>\n";
+                $html .=  "<div>\n";
+                for( $j=0 ; $j < $colone ; $j++ )
+                {
+                    // la colonne est-elle pair ?
+                    if ( $j % 2 == 0 )
+                        $class="class='noir'";
+                    else
+                        $class="class='blanc'";
+                    $html .= "<div $class>\n";
+                    $html .= "</div>\n";
+                }
+                $html .= "</div>\n";
             }
-            $html .= "</tr>\n";
-        }
-        // sinon
-        else
-        {
-            $html .= "<tr>\n";
-            for( $j=0 ; $j < $colone ; $j++ )
+            // sinon
+            else
             {
-                if ( $j % 2 == 0 )
-                    $class="class='blanc'";
-                else
-                    $class="class='noir'";
-                    $html .= "<td $class>\n";
-                $html .= "</td>\n";
+                $html .= "<div>\n";
+                for( $j=0 ; $j < $colone ; $j++ )
+                {
+                    if ( $j % 2 == 0 )
+                        $class="class='blanc'";
+                    else
+                        $class="class='noir'";
+                    $html .= "<div $class>\n";
+                    $html .= "</div>\n";
+                }
+                $html .= "</div>\n";
             }
-            $html .= "</tr>\n";
         }
-    }
-            $html .="</table>\n";
+        $html .= "</div>";
+
         return $this->render('demo/damier.html.twig', [
             'damier' => $html,
+        ]);
+    }
+
+    public function message(): Response
+    {
+        $message = new Message();
+
+        $form = $this->createForm(FormulaireType::class, $message);
+
+        return $this->render('demo/formulaire.html.twig', [
+            'monFormulaire' => $form->createView(),
         ]);
     }
 }
